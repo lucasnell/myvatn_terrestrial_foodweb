c87530d1eb1716fb4e6a5199988ebe965e80ccdd
Unknown <jsphillips2@wisc.edu>
2017-11-13 13:32:36 -0600

    Cleaned up to facilitate dispaly; fixed error in 'initial_states'

--- aaa-class.R
+++ aaa-class.R
@@ -10,15 +10,19 @@ web <- R6Class(
     public = list(
         
         # Desired equilibrium values
-        Neq = 343000, Deq = 114000, Peq = 4300, Veq = 81, Heq = 24, Req = 13, Meq = 0,
+        Neq = 343000, Deq = 114000, Peq = 4300, 
+        Veq = 81, Heq = 24, Req = 13, Meq = 0,
         # Initial states
-        N0 = NULL, D0 = NULL, P0 = NULL, V0 = NULL, H0 = NULL, R0 = NULL, M0 = NULL,
+        N0 = NULL, D0 = NULL, P0 = NULL, V0 = NULL, 
+        H0 = NULL, R0 = NULL, M0 = NULL,
         # Inputs
         iN = 1000,
         # Loss rates from systems
-        lD = 0.1, lP = 0.1, lV = 0.1, lH = 0.1, lR = 0.1, lM = 0.1,
+        lD = 0.1, lP = 0.1, lV = 0.1, lH = 0.1, 
+        lR = 0.1, lM = 0.1,
         # Loss rates from pool (returned to either N or D)
-        mN = NA, mP = NA, mD = 0.1, mV = 0.1, mH = 0.1, mR = 0.1, mM = 0.5,
+        mN = 0.002, mP = NA, mD = NA, mV = 0.1, mH = 0.1, 
+        mR = 0.1, mM = 0.5,
         # Carying Capacities
         kP = 8000, 
         kV = 162, kH = 48, kR = 26,  # only for model A
@@ -26,12 +30,14 @@ web <- R6Class(
         hP = 1, hD = 1, hR = 1,      # only for model B
         # Uptake rates
         aNP = NA, aDV = NA, aPH = NA, aR = NA,
-        # Midge function (have to wrap inside a list bc it's unchangeable otherwise)
+        # Midge function 
+        #(have to wrap inside a list bc it's unchangeable otherwise)
         iM_func = list(function(t) 0),
         # Model A or B
         model = 'A',
         
-        initialize = function(..., do_solve = TRUE, initial_vals = rep(0.1, 6)) {
+        initialize = function(..., do_solve = TRUE, 
+                              initial_vals = rep(0.1, 6)) {
             
             # Checking types
             pars <- list(...)
@@ -39,26 +45,33 @@ web <- R6Class(
                 for (i in 1:length(pars)) {
                     if (names(pars)[i] == 'iM_func') {
                         if (!is.function(pars[[i]])) {
-                            stop("parameter iM_func must be a function")
-                        } else if (!identical(formalArgs(pars[[i]]), 't')) {
-                            stop("parameter iM_func must be a function with the only ",
+                            stop("parameter iM_func must be a
+                                 function")
+                        } else if (!identical(formalArgs(pars[[i]]),
+                                              't')) {
+                            stop("parameter iM_func must be a",
+                                 "function with the only ",
                                  "argument being 't'")
                         }
                     } else if (names(pars)[i] == 'model') {
                         if (! pars[[i]] %in% LETTERS[1:2]) {
                             stop("model must be 'A' or 'B'")
                         }
-                    } else if (!is.na(pars[[i]]) & !is.numeric(pars[[i]])) {
-                        stop("parameter ", names(pars)[i], " is not either NA or numeric, ",
+                    } else if (!is.na(pars[[i]]) & !is.numeric
+                               (pars[[i]])) {
+                        stop("parameter ", names(pars)[i], " is not",
+                             "either NA or numeric, ",
                              "as is required")
                     }
                 }
             }
-            # Filling pars list with default values when they're not supplied
+            # Filling pars list with default values 
+            # (when they're not supplied)
             defaults = private$par_list()
             pars[names(defaults)[!names(defaults) %in% names(pars)]] = 
                 defaults[!names(defaults) %in% names(pars)]
-            # If an initial state is NULL, use that pool's equilibrium value
+            # If an initial state is NULL, 
+            # use that pool's equilibrium value
             sv = private$pool_names()
             for (s in sv) {
                 if (is.null(pars[[paste0(s, '0')]])) {
@@ -68,22 +81,26 @@ web <- R6Class(
             
             # Checking for too many unknown values
             if (length(pars[is.na(pars)]) != 6 & do_solve) {
-                stop("cannot solve for unknown parameters when the number of unknown ",
+                stop("cannot solve for unknown parameters when the",
+                     "number of unknown ",
                      "(i.e., NA) parameters is != 6; ",
                      "by default the following are NA: ",
-                     "mN, mP, aNP, aDV, aPH, and aR")
+                     "mP, mD, aNP, aDV, aPH, and aR")
             }
             if (length(pars[is.na(pars)]) > 0 & !do_solve) {
-                stop("if not solving for unknown parameters, you cannot have any ",
+                stop("if not solving for unknown parameters, you",
+                     "cannot have any ",
                      "parameters as NA; ",
                      "by default the following are NA: ",
-                     "mN, mP, aNP, aDV, aPH, and aR")
+                     "mP, mD, aNP, aDV, aPH, and aR")
             }
             
-            # Use estimated equilibrium biomass and selected values for certain 
+            # Use estimated equilibrium biomass 
+            # and selected values for certain 
             # parameters to solve for unknown values
-            # "Known" parameters must be selected with care, to ensure that an 
-            # equilibrium solution can actually be reached
+            # "Known" parameters must be selected with care, 
+            # to ensure that an equilibrium solution can 
+            # actually be reached
             if (do_solve) {
                 
                 start = initial_vals
@@ -92,9 +109,13 @@ web <- R6Class(
                 par_solve = multiroot(f = private$equil_solve, 
                                       start = start, parms = pars,
                                       maxiter = 100,
-                                      rtol = 1e-6, atol = 1e-8, ctol = 1e-8,
-                                      useFortran = TRUE, positive = TRUE, jacfunc = NULL,
-                                      jactype = "fullint", verbose = FALSE, bandup = 1,
+                                      rtol = 1e-6, atol = 1e-8, 
+                                      ctol = 1e-8,
+                                      useFortran = TRUE, 
+                                      positive = TRUE, 
+                                      jacfunc = NULL,
+                                      jactype = "fullint", 
+                                      verbose = FALSE, bandup = 1,
                                       banddown = 1)
                 
                 pars[names(par_solve$root)] = as.list(par_solve$root)
@@ -104,12 +125,15 @@ web <- R6Class(
         },
         
         
-        re_solve = function(solve_pars = c("mN", "mP", "aNP", "aDV", "aPH", "aR"),
+        re_solve = function(solve_pars = c("mP", "mD", "aNP", "aDV",
+                                           "aPH", "aR"),
                             initial_vals = rep(0.1, 6)) {
             
-            stopifnot(length(solve_pars) == 6, length(initial_vals) == 6)
+            stopifnot(length(solve_pars) == 6, 
+                      length(initial_vals) == 6)
             if (! all(solve_pars %in% private$par_names())) {
-                stop("One or more solve parameter names is not present in this class.")
+                stop("One or more solve parameter names is not",
+                     "present in this class.")
             }
             
             pars = private$par_list()
@@ -121,9 +145,13 @@ web <- R6Class(
             par_solve = multiroot(f = private$equil_solve, 
                                   start = start, parms = pars,
                                   maxiter = 100,
-                                  rtol = 1e-6, atol = 1e-8, ctol = 1e-8,
-                                  useFortran = TRUE, positive = TRUE, jacfunc = NULL,
-                                  jactype = "fullint", verbose = FALSE, bandup = 1,
+                                  rtol = 1e-6, atol = 1e-8, 
+                                  ctol = 1e-8,
+                                  useFortran = TRUE, 
+                                  positive = TRUE, 
+                                  jacfunc = NULL,
+                                  jactype = "fullint", 
+                                  verbose = FALSE, bandup = 1,
                                   banddown = 1)
             
             pars[names(par_solve$root)] = as.list(par_solve$root)
@@ -135,17 +163,20 @@ web <- R6Class(
         
         
         # Function to Solve ODE
-        # tmin and tmax specify the duration over which to run the model
+        # tmin and tmax specify the duration over which to 
+        # run the model
         # tstep specifies the step size
         ode_solve = function(tmin, tmax, tstep){
             
-            # parms and init give the parameter values and initial states
+            # parms and init give the parameter values and 
+            # initial states
             parms = private$par_list()
             
             init = unlist(parms[paste0(private$pool_names(), '0')])
             names(init) = private$pool_names()
             
-            solved_ode = ode(init, seq(tmin,tmax,tstep), private$diff_eq, parms)
+            solved_ode = ode(init, seq(tmin,tmax,tstep), 
+                             private$diff_eq, parms)
             solved_ode = as_tibble(as.data.frame(solved_ode))
             
             return(solved_ode)
@@ -169,18 +200,23 @@ web <- R6Class(
             cat('  - X0: starting value for pool X\n')
             cat('  - Xeq: desired equilibrium value for pool X\n')
             cat('  - iN: input to soil nutrient pool\n')
-            cat('  - lX: loss rates from system from pool X (X: D,P,V,H,R,M)\n')
-            cat('  - mX: loss rates from pool X, returned to either N or D\n',
+            cat('  - lX: loss rates from system from pool X 
+                (X: D,P,V,H,R,M)\n')
+            cat('  - mX: loss rates from pool X, returned to either 
+                N or D\n',
                 '   (X: N,D,P,V,H,R,M)\n')
             if (self$model == 'A') {
-                cat('  - kX: carrying capacities for pool X (X: P,V,H,R)\n')
+                cat('  - kX: carrying capacities for pool X 
+                    (X: P,V,H,R)\n')
             } else {
                 cat('  - kX: carrying capacities for pool X (X: P)\n')
                 cat('  - hX: handling time for pool X (X: P,D,R)\n')
                 
             }
-            cat('  - aNP: maximum uptake rate from pool X to pool Y (XY: NP, CV, PH)\n')
-            cat('  - aR: maximum uptake rate to predators; same value for V,H,M\n')
+            cat('  - aNP: maximum uptake rate from pool X to pool Y
+                (XY: NP, CV, PH)\n')
+            cat('  - aR: maximum uptake rate to predators; 
+                same value for V,H,M\n')
             cat('  - iM_func: function for midge input at time t\n')
         }
         
@@ -333,8 +369,8 @@ web <- R6Class(
                                     P = aNP*N*P*(1-P/kP) - aPH*P*H*(1-H/kH) - mP*P,
                                     V = aDV*D*V*(1-V/kV) - (aR*V*R)*(1-R/kR) - mV*V,
                                     H = aPH*P*H*(1-H/kH) - (aR*H*R)*(1-R/kR) - mH*H,
-                                    R = (aR*V*R + aR*H*R + 0.5*aR*M*R)*(1-R/kR) - mR*R,
-                                    M = iM - mM*M - (0.5*aR*M*R)*(1-R/kR))
+                                    R = (aR*V*R + aR*H*R + aR*M*R)*(1-R/kR) - mR*R,
+                                    M = iM - mM*M - (aR*M*R)*(1-R/kR))
                               }))
             } else {
                 output = 
@@ -364,9 +400,9 @@ web <- R6Class(
     active = list(
         
         initial_states = function(){
-            data.frame(pool=c("N","D","V","P","H","R","M"), 
-                       biomass = self %>% with(c(N0,D0,V0,P0,H0,R0,M0))
-                       )
+            data.frame(pool = c("N","D","V","P","H","R","M"), 
+                       biomass =  c(self$N0,self$D0,self$V0,self$P0,self$H0,self$R0,self$M0))
+                
         }
         
     )
