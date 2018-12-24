34facd151a783f6336490b6db86dd15671396e38
Lucas Nell <lucas@lucasnell.com>
2018-12-24 15:26:56 -0500

    Built pkg, fixed error in par_estimates where it was full of NAs

--- data-raw/par_estimates.R
+++ data-raw/par_estimates.R
@@ -7,8 +7,14 @@ tibbles <- map(list.files("data-raw/parameters", "*.csv", full.names = TRUE),
                ~ read_csv(.x))
 names(tibbles) <- gsub(".csv", "", list.files("data-raw/parameters", "*.csv"))
 
-# Fix mistake in original file
-# tibbles$full_pars <- tibbles$full_pars %>% rename(mD = `\n            mD`)
+# Function to fill NAs in columns with only 1 non-NA value.
+# Other columns are unchanged
+fill_nas <- function(x) {
+    if (sum(is.na(x)) == length(x) - 1) {
+        return(rep(x[!is.na(x)], length(x)))
+    }
+    return(x)
+}
 
 par_estimates <- full_join(tibbles$full_pars,
                            tibbles$iN_eqs, by = "iN") %>%
@@ -18,7 +24,7 @@ par_estimates <- full_join(tibbles$full_pars,
                      paste0(c("N", "D", "P", "V", "H", "R"), "eq"))) %>%
     select(V, H, R, iN, everything()) %>%
     arrange(desc(V), desc(H), desc(R), desc(iN)) %>%
-    mutate_at(vars(matches("^l|^m|^a|^k")), mean, na.rm = TRUE) %>%
+    mutate_all(fill_nas) %>%
     mutate_at(vars(V, H, R), as.integer)
 
 # usethis::use_data(par_estimates, overwrite = TRUE)
